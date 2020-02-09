5f1360a378e3617af3db7319abcedf3c9804ffc2
axel <mmowismer@outlook.com>
2020-02-09 14:24:50 -0300

    initial

---
+++ exams/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Exam(models.Model):
+    class Meta:
+        verbose_name = 'Examen'
+        verbose_name_plural = 'Examenes'
+
+    title = models.CharField(u'Titulo', max_length=256)
+    solution = models.TextField(u'Solucion')
+    evaluation_text = models.TextField(u'Texto de evaluacion')
+
+    def __str__(self):
+        return self.title
+
+
+class Word(models.Model):
+    class Meta:
+        verbose_name = 'Palabra'
+        verbose_name_plural = 'Palabras'
+
+    word = models.CharField(u'Palabra', max_length=10)
+    prefix = models.CharField(u'Prefijo', max_length=30)
+    solution = models.CharField(u'Solucion', max_length=10)
+    exam = models.ForeignKey("Exam", on_delete=models.CASCADE, verbose_name='Examen')
+
+    def __str__(self):
+        return self.word
